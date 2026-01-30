cc166aa797817271f915b5fcb95b093e862f76ca
Júlio César <juliocesarddn@gmail.com>
2026-01-30 10:02:40 -0300

    testando o if

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-         
+            int idadeJulio = 22;
+            int idadeGiovanna = 29;
+         if (idadeJulio > idadeGiovanna)
+            {
+                Console.WriteLine("Julio é mais velhor que Giovanna");
+            }else
+            {
+                Console.WriteLine("Giovanna é mais velha que Júlio");
+            }
         }
     }
 }
